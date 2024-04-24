c132c41f31aef78952444bbe2fe9d22b6e7eef94
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-04-24 17:08:09 +0200

    Add clickcount to button online

--- js/script.js
+++ js/script.js
@@ -2,7 +2,33 @@
 
 window.addEventListener("load", initialise);
 
+let ancestry, buttonOnline, clickCountOnline;
 function initialise() {
+    //Variables
+    ancestry = document.getElementById("ancestery");
+    buttonOnline = document.getElementById("get-data-online");
+    clickCountOnline = 0;
 
+    //event and functions
+    buttonOnline.addEventListener("click", isOnline);
 }
 
+function fillAncestry(){
+    
+    if(clickCountOnline % 2 == 0 ){
+
+    }
+    else{
+
+    }
+}
+
+function isOnline(){
+    clickCountOnline += 1
+    if(clickCountOnline % 2 == 0 ){
+        buttonOnline.innerText = "Offline";
+    }
+    else{
+        buttonOnline.innerText = "Online";
+    }
+}

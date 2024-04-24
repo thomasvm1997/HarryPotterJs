8e17f268de42c481c04f2f93f15888275a460476
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-04-25 00:22:38 +0200

    Add filter to radiobuttons and options

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 
 window.addEventListener("load", initialise);
 
-let ancestry, buttonOnline, clickCountOnline, houseSelectBox;
+let ancestry, buttonOnline, clickCountOnline, houseSelectBox, cardContainer, radioButtons, filteredCharacters;
+
 const ancesteryArrayOffline = ["All", "half-blood","muggleborn", "pure-blood"];
 const ancesteryArrayOnline = ["All", "half-blood","muggleborn", "pure-blood", "squib", "muggle", "half-veela", "quarter-veela"];
 const houseArray = ["All", "Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
@@ -12,40 +13,51 @@ function initialise() {
     ancestry = document.getElementById("ancestry");
     buttonOnline = document.getElementById("get-data-online");
     houseSelectBox = document.getElementById("houses")
+    cardContainer = document.querySelector(".cardContainer")
+    
     clickCountOnline = 0;
     
     //event and functions
     fillHouses();
     buttonOnline.addEventListener("click", isOnline);
+    
+    
 }
 
 function fillAncestry(){
-    
+    ancestry.replaceChildren();// lijst leegmaken
+    const container = document.createElement("div");
+    container.className = "RadioButtonContainer";
+    let value = 0;
     if(clickCountOnline % 2 == 0 ){ //offline
-        ancestry.replaceChildren();// lijst leegmaken
-
+        
         ancesteryArrayOffline.forEach(c => {
             const radioButton = document.createElement("input");
             radioButton.type = "radio";
-            radioButton.className = "radioButton"; //hier gaan we de radiobutton aanmaken: rechtsreeks werken met `<input type="radio" checked /><label>${c}</label>` lukte niet.
+            radioButton.id = c; //hier gaan we de radiobutton aanmaken: rechtsreeks werken met `<input type="radio" checked /><label>${c}</label>` lukte niet.
+            radioButton.name = "buttonAncestry"; //Zorgt ervoor dat de andere auto deselect wordt
+            radioButton.value = value++;
+            radioButton.addEventListener("click", filterCharacters)
             if(c == "All"){ 
                 radioButton.checked = true;
             }                                        
-                                                   
+            
+            
+
             const label = document.createElement("label");
             label.textContent = c; //naam van de het element uit de lijst: we loopen door de lijst.
 
-            const container = document.createElement("div");
-            container.className = "RadioButtonContainer";
+            
             container.appendChild(radioButton);
             container.appendChild(label);
-            ancestry.appendChild(container);//We steken alles in de container en daarna container in ancestry
-
+            
         });
     }
     else{//online
-        ancestry.replaceChildren();
+        
     }
+    ancestry.appendChild(container);//We steken alles in de container en daarna container in ancestry
+    radioButtons = document.querySelector('input[type="radio"]');
 }
 
 
@@ -63,8 +75,29 @@ function isOnline(){
 function fillHouses(){
     houseArray.forEach(c => {
         const option = document.createElement("option");
-        option.value = c;
+        option.id = c;
         option.textContent = c;
+        option.addEventListener("change", filterCharacters)
         houseSelectBox.appendChild(option);
     });
 }
+
+function filterCharacters(){
+    let id = this.getAttribute("id");
+    
+    if(this.getAttribute("type") == "radio"){
+    
+        filteredCharacters = potterCharacters.filter(c => c.ancestry == id);
+    
+    }
+    else{
+        filteredCharacters = potterCharacters.filter(c => c.house == id);
+    }
+    //functie die cards maakt hier
+}
+
+function makeCharacterCards(){
+    
+}
+
+

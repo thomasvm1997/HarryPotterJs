5ac0cce00fda6c0dc3d7ff603f1c7a6c632ac51f
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2024-04-27 19:23:56 +0200

    Remove code duplication for json array

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
 window.addEventListener("load", initialise);
 
 let ancestry, buttonOnline, clickCountOnline, houseSelectBox, cardContainer, radioButtons, filteredCharacters,
-houseValue, ancestryId;
+    houseValue, ancestryId, arrayJson;
 
-const ancesteryArrayOffline = ["All", "half-blood","muggleborn", "pure-blood"];
-const ancesteryArrayOnline = ["All", "half-blood","muggleborn", "pure-blood", "squib", "muggle", "half-veela", "quarter-veela"];
+const ancesteryArrayOffline = ["All", "half-blood", "muggleborn", "pure-blood"];
+const ancesteryArrayOnline = ["All", "half-blood", "muggleborn", "pure-blood", "squib", "muggle", "half-veela", "quarter-veela"];
 const houseArray = ["All", "Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 
 function initialise() {
@@ -20,64 +20,68 @@ function initialise() {
     clickCountOnline = 0;
     
     //event and functions
-    fillAncestry();
+    logCharacters();
+    fillAncestry(ancesteryArrayOffline);
     fillHouses();
     filterCharacters();
     buttonOnline.addEventListener("click", isOnline);
-    
-    
+
+
 }
 
-function fillAncestry(){
+function fillAncestry(param) { //KIJKEN OM DEZE TE LATEN WERKEN MET PARAMETER ARRAY!!! Probeersel
     ancestry.replaceChildren();// lijst leegmaken
     const container = document.createElement("div");
     container.className = "RadioButtonContainer";
     let value = 0;
-    if(clickCountOnline % 2 == 0 ){ //offline
-        
-        ancesteryArrayOffline.forEach(c => {
+    
+
+        param.forEach(c => {
             const radioButton = document.createElement("input");
             radioButton.type = "radio";
             radioButton.id = c; //hier gaan we de radiobutton aanmaken: rechtsreeks werken met `<input type="radio" checked /><label>${c}</label>` lukte niet.
             radioButton.name = "buttonAncestry"; //Zorgt ervoor dat de andere auto deselect wordt
             radioButton.value = value++;
-            radioButton.addEventListener("click",function(){ancestryId = this.getAttribute("id");filterCharacters();});
-            if(c == "All"){ 
+            radioButton.addEventListener("click", function () { ancestryId = this.getAttribute("id"); filterCharacters(); });
+            if (c == "All") {
                 radioButton.checked = true;
-            }                                        
-            
-            
+            }
+
+
 
             const label = document.createElement("label");
             label.textContent = c; //naam van de het element uit de lijst: we loopen door de lijst.
 
-            
+
             container.appendChild(radioButton);
             container.appendChild(label);
-            
-        });
-    }
-    else{//online
+
         
-    }
-    ancestry.appendChild(container);//We steken alles in de container en daarna container in ancestry
+
+    });
+
+    ancestry.appendChild(container);
     radioButtons = document.querySelector('input[type="radio"]');
 }
 
 
-function isOnline(){
-    clickCountOnline += 1
-    if(clickCountOnline % 2 == 0 ){
+function isOnline() {
+    clickCountOnline += 1;
+    
+    if (clickCountOnline % 2 == 0) {
         buttonOnline.innerText = "Offline";
+        fillAncestry(ancesteryArrayOffline);
     }
-    else{
+    else {
         buttonOnline.innerText = "Online";
+        fillAncestry(ancesteryArrayOnline);
     }
-    fillAncestry();
+    
 }
 
-function fillHouses(){
-    
+
+function fillHouses() {
+
     houseArray.forEach(c => {
         const option = document.createElement("option");
         option.id = c;
@@ -86,45 +90,38 @@ function fillHouses(){
         //option.addEventListener("input", filterCharacters)
         houseSelectBox.appendChild(option);
     });
-    houseSelectBox.addEventListener("input", function(){houseValue = this.value; filterCharacters()});
+    houseSelectBox.addEventListener("input", function () { houseValue = this.value; filterCharacters() });
 }
 
-function filterCharacters(){
-    
-    
+function filterCharacters() {
+
+
     console.log(`Id:${ancestryId}`);
     console.log(`HouseValue:${houseValue}`);
-    
-/*     if(this.getAttribute("type") == "radio"){
-    
-        filteredCharacters = potterCharacters.filter(c => c.ancestry == id);
-    
-    }
-    else{
-        filteredCharacters = potterCharacters.filter(c => c.house == houseValue);
-    } */
-    if(houseValue == "All" || ancestryId == "All"){
-        if(houseValue == "All" && ancestryId != "All"){
+
+
+    if (houseValue == "All" || ancestryId == "All") {
+        if (houseValue == "All" && ancestryId != "All") {
             filteredCharacters = potterCharacters.filter(c => c.ancestry == ancestryId)
         }
-        else if(ancestryId == "All" && houseValue != "All"){
+        else if (ancestryId == "All" && houseValue != "All") {
             filteredCharacters = potterCharacters.filter(c => c.house == houseValue)
         }
-        else{
+        else {
             filteredCharacters = potterCharacters;
         }
-        
+
     }
-    else{
+    else {
         filteredCharacters = potterCharacters.filter(c => c.ancestry == ancestryId && c.house == houseValue);
-        
+
     }
-    
+
     makeCharacterCards();
-    
+
 }
 
-function makeCharacterCards(){
+function makeCharacterCards() {
     const cardContainer = document.querySelector("main");
     cardContainer.replaceChildren();
     filteredCharacters.forEach(c => {
@@ -133,7 +130,7 @@ function makeCharacterCards(){
 
         const cardImage = document.createElement("img");
         cardImage.src = c.image;
-        cardImage.title = `Wizard: ${c.wizard == true ? "Yes": "No"}\nStatus: ${c.hogwartsStudent == true ? "Student": "Staff"}`;
+        cardImage.title = `Wizard: ${c.wizard == true ? "Yes" : "No"}\nStatus: ${c.hogwartsStudent == true ? "Student" : "Staff"}`;
 
         const nameCharacter = document.createElement("p");
         nameCharacter.className = "nameCharacter";
@@ -142,7 +139,7 @@ function makeCharacterCards(){
         const nameActor = document.createElement("p");
         nameActor.className = "actorName";
         nameActor.innerText = c.actor;
-        
+
         card.appendChild(cardImage);
         card.appendChild(nameCharacter);
         card.appendChild(nameActor);
@@ -153,3 +150,12 @@ function makeCharacterCards(){
 }
 
 
+
+
+
+async function logCharacters() {
+    const response = await fetch("https://thomasvm23.github.io/super-octo-guide/characters.json");
+    arrayJson = await response.json();
+
+    console.log(arrayJson);
+}
